cf7df39dfa498ff8e6270e4dff43afc788545940
naingminkhant <naingminkhant.cm@gmail.com>
2017-05-24 16:36:44 +0630

    Initial

--- vostro/calculator/src/CalculatorServiceProvider.php
+++ vostro/calculator/src/CalculatorServiceProvider.php
@@ -13,7 +13,7 @@ class CalculatorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        include __DIR__.'/routes.php';
+        include __DIR__ . '/routes.php';
     }
 
     /**
