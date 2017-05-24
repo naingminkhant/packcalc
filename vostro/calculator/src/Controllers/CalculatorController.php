6e0e43e8140f7ab26134f8600b742c768e62079f
naingminkhant <naingminkhant.cm@gmail.com>
2017-05-24 16:19:47 +0630

    Sample Init

---
+++ vostro/calculator/src/Controllers/CalculatorController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Vostro\Calculator\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CalculatorController extends Controller
+{
+    public function add($a,$b){
+        echo $a+$b;
+    }
+}
